b3876b5a06cb996585aad3abd34dfda0447e9167
Sue Lee <sue.lee0308@gmail.com>
2021-09-30 16:01:04 -0500

    saveEvent works with this. retrieving from local storage works. readme updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # day-planner
+
+## Description
+A simple calendar application that allows a user to save events for each hour of the business day. This app will run in the browser and feature dynimcally updated HTML and CSS powered by jQuery. The Moment.js library is used to display the date and time.
+
+## Credit
+Classmate Ethan helped with understanding DOM traversal and how that "might" apply to this assignment!
+
+## Links:
+- GitHub Repo:https://github.com/suelee0308/day-planner
+- Deployed webpage:https://suelee0308.github.io/day-planner/
+
+## Screenshot:
+![screenshot](./assets/images/screenshot.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,45 +7,35 @@ var localInput = localStorage.getItem("plannedEvent");
 var keyName;
 
 
-
 // show current day at the top in this format: DoW, Month day
 $("#currentDay").text(moment.format("dddd, MMMM Do, YYYY"));
 
 // store textarea text into local storage
-saveBtn.on("click", saveEvent);
+$(".saveBtn").on("click", saveEvent);
 
-function saveEvent(event) {
-    var clickBtn = $(event.target);
-    console.log(clickBtn);
-    var userEvent = clickBtn.siblings("input").val();
+function saveEvent(event) {  
+    var userEvent = $(this).siblings("input").val();
     console.log(userEvent);
-    var keyName = $(event.target).attr("id");
+    var keyName = $(this).attr("id");
     console.log(keyName);
     localStorage.setItem(keyName, userEvent);
 };
 
 
 // function to retrieve date from local storage at refresh
-$("button").each(function(){
-    var keyID = $(this).attr("id");
-    console.log(keyID);
-    console.log(typeof keyID);
-    var storedEvent = localStorage.getItem(keyID);
-    console.log(storedEvent);
+// $("#9").siblings("input").val(localStorage.getItem("9"));
 
-    $("#"+keyID).val(storedEvent)
-        // if(storedEvent) {
-        //     $("#"+keyID).each(function(){
-        //         plannedEvent.val(storedEvent)
-        //     });
-        // }
-})
-
+$(document).ready(function(){ 
+    $("button").each(function(){
+        for (var i = 9; i <= 17; i++){
+        $("#"+i).siblings("input").val(localStorage.getItem(""+i))
+        }
+    })
+});
 
 
 console.log(currentHour);
 console.log(typeof currentHour);
-
 // in each timeblock, if the time (value which is given through data attributes) < hour { set class /attribute to .past, remove attribute .present .future}
 // in each timeblock, if the time > dateShown {set attribute to .future, remove attribute .past .present}
 // in each timeblock, if the moment h === dateshown hour {set attribute .present}
@@ -66,67 +56,3 @@ $("input").each(function(){
         $(this).removeClass('future');
     }
 });
-
-// var keyID = $("row").children().eq(3).attr("id");
-// console.log(keyID);
-// var fromLocal = localStorage.getItem(10);
-// console.log(fromLocal);
-// $(function(){ 
-//     $("button").each(function() {
-//         var id = $(this).attr("id");
-//         console.log(id);
-//         if (keyID == id) {
-//         plannedEvent.text = localStorage.getItem(keyID)
-//         }
-//     });
-// });
-
-// $(document).ready(function(){
-
-// })
-// function ()
-// if (this.find("id").val()
-
-
-
-
-// this.timblock.child.child(textarea).value has a atext, then run function onload
-// else textconent = ""
-// if there is data in local storage
-
-// var currentTime = moment();
-// var time = function(){
-//     currentTime.format("h:mm")};
-// setInterval(time, 60000);
-// console.log("current time" + currentTime);
-
-
-// var timesHour = function(){
-//     currentHour.hour()};
-// setInterval(currentHour, 1000*60*60);
-// console.log("current hour" + currentHour);
-
-
-// time blocks for standard business hours = 9-17
-
-
-// way to call the value in the id of each time
-// event.target.$("id")
-
-// each middle block needs to be an input/textarea field where the button is a save button
-// save button needs to store text written in local storage (check to make sure it saved when refreshed)
-// function for time block changing color depending on if it's in the past/present/future
-
-
-// in each timeblock, if the time (value which is given through data attributes) < hour { set class /attribute to .past, remove attribute .present .future}
-// in each timeblock, if the time > dateShown {set attribute to .future, remove attribute .past .present}
-// in each timeblock, if the moment h === dateshown hour {set attribute .present}
-
-
-
-// save button - on click grab text from textarea and store locally.
-// in each text area linked to its time block, the text content needs to load the locally stored value for whatever time block its stored in, use id's as keys of the value
-
-
-// setInterval needs a function passed in order to work
-
